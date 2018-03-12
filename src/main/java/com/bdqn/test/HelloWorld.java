e02cd4b28af2cc1b6e40b7fb1185589b08cc17eb
MyBlackBird <469050880@qq.com>
2018-03-12 15:02:55 +0800

    GitTest项目

---
+++ src/main/java/com/bdqn/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.bdqn.test;
+
+/**
+ * Created by houlingfeng on 2018/3/12.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hellworld");
+    }
+}
